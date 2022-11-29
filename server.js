595df88df3883738463a5999d4ee8d2731d59fc5
Jaime Alberto Esquivel Acosta <jaimeesquivel@Jaimes-MacBook-Pro.local>
2022-11-29 12:18:59 -0600

    server.js functional version

--- server.js
+++ server.js
@@ -1,22 +1,66 @@
 const express = require('express');
 const path = require('path');
-//const api = require('./routes/index.js');
+const fs = require('fs');
+const util = require('util');
 
 const app = express();
-
 const PORT = 3001;
 
-// Ruta GET para la página de inicio
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.use(express.static('./public'));
 
-// Ruta GET para la página de retroalimentación
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
+//HTML endpts
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
+app.get('/notes', function(req, res){
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} ðŸš€`)
-);
+//
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
+// GET Request
+app.get('/api/notes', function(req, res){
+
+  readFile('./db/db.json', 'utf8').then(
+    
+    function(data){
+
+      notes = [].concat(JSON.parse(data));
+      res.json(notes);
+
+    })
+
+});
+
+//POST Request
+app.post('/api/notes', function(req, res){
+
+  const note = req.body;
+
+  readFile('./db/db.json', 'utf8').then(
+
+    function(data){
+
+      const notes = [].concat(JSON.parse(data));
+      note.id = notes.length + 1
+      notes.push(note);
+      return notes;
+
+    }).then(function(notes){
+
+      writeFile('./db/db.json', JSON.stringify(notes))
+      res.json(note);
+
+    });
+
+});
+
+//Listen
+app.listen(PORT, function(){
+  console.log(`Server listening on PORT ${PORT}`);
+});
