4ec364c33eaf91646ecb6b70861f72d9e247d266
Jaime Alberto Esquivel Acosta <jaimeesquivel@Jaimes-MacBook-Pro.local>
2022-11-29 12:49:26 -0600

    PORT err on heroku

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const util = require('util');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
